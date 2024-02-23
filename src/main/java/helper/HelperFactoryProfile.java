ecaf3e3e80e9734916df1d9cd97012216ecfa9c7
olga-82 <134201905+olga-82@users.noreply.github.com>
2024-02-23 09:24:38 +0200

    add

--- src/main/java/helper/HelperFactoryProfile.java
+++ src/main/java/helper/HelperFactoryProfile.java
@@ -18,7 +18,49 @@ public class HelperFactoryProfile extends  HelperBase{
     private final String INPUT_INSTAGRAM =" //*[@id='root']//div[2]/div[3]/div[4]/div[3]/input";
     private final String INPUT_LINKEDIN =" //*[@id='root']//div[2]/div[3]/div[4]/div[4]/input";
 
+//*[@class='sc-jxgRln JHglX'] now
+//*[@id="root"]//div[2]/div[3]/div[5]/div/div[2]/div/div next week
+//*[@id="root"]//div[2]/div[3]/div[5]//div[3]/div/div next month
+    //*[@id="root"]//div[2]/div[3]/div[6]/input industry
+    //*[@id="root"]//div[2]/div[3]/div[7]/input keywords
+    //*[@id='AboutProvider'] about
+    //*[@id="root"]//div[2]/div[5]/div[1]/input founded
+    //*[@id="root"]//div[2]/div[5]/div[2]/input  language
+    //*[@id="root"]//div[2]/div[5]/div[3]/input shipping to
+    //*[@id="root"]//div[2]/div[5]/div[1]/input payments terms
+    //*[@id="root"]/div[1]/div[2]/div[6]/div[1]/div/div[1]/div/div/div cust yes
+    //*[@id="root"]/div[1]/div[2]/div[6]/div[1]/div/div[2]/div/div/div  cust no
+    //*[@id="root"]/div[1]/div[2]/div[6]/div[2]/div/div[1]/div/div/div yes
+    //*[@id="root"]/div[1]/div[2]/div[6]/div[2]/div/div[2]/div/div/div no
+    //*[@id="root"]/div[1]/div[2]/div[6]/div[3]/div/div[1]/div/div/div yes
+    //*[@id="root"]/div[1]/div[2]/div[6]/div[3]/div/div[2]/div/div/div no
+    //*[@id="root"]/div[1]/div[2]/div[6]/div[4]/div/div[1]/div/div/div yes
+    //*[@id="root"]/div[1]/div[2]/div[6]/div[4]/div/div[2]/div/div/div no
+    //*[@id="root"]/div[1]/div[2]/div[6]/div[5]/div/div[1]/div/div/div yes
+    //*[@id="root"]/div[1]/div[2]/div[6]/div[5]/div/div[2]/div/div/div no
+    //*[@id="root"]/div[1]/div[2]/div[6]/div[6]/div/div[1]/div/div/div yes
+    //*[@id="root"]/div[1]/div[2]/div[6]/div[6]/div/div[2]/div/div/div no
+    //*[@id="root"]/div[1]/div[2]/div[7]/button button preview
+    //*[@id="root"]/div[1]/div[2]/div[2]/div[1]/button return toe editing
+    //*[@id="root"]/div[1]/div[2]/div[2]/div[2]/button save
+    //div[@class='sc-iQviOx hkGxeg']//div[@class='sc-fePcEy cZfHCq']//*[name()='svg'] edit product
+    //div[@class='sc-iQviOx hkGxeg']//div[@class='sc-iaEFAN iNUINm']//*[name()='svg']
+    //*[@id="root"]/div[1]/div[2]/div[3]/div[2]/div[1]/div/div[1] add product
+    //div[@class='sc-iOTpNu kjTrFZ']//*[name()='svg'] close product
 
+    //input[@value='No name product'] name product
+    //*[@id="root"]/div[1]/div[2]/div[3]/div[2]/div[1]/div/div[3]/div[4]/input quantity
+    //*[@id="root"]/div[1]/div[2]/div[3]/div[2]/div[1]/div/div[3]/div[5]/input prise
+    //button[@class='sc-iyjcSV lkrIHE'] upload foto
+    //div[@class='sc-iOTpNu kjTrFZ']//*[name()='svg'] clouse form
 
+    //div[@class='sc-HUpva nvGQK']//div[@class='sc-fePcEy cZfHCq']//*[name()='svg'] add company
+    //div[@class='sc-iOTpNu kjTrFZ']//*[name()='svg'] close company
+    //div[@class='sc-HUpva nvGQK']//div[@class='sc-iaEFAN iNUINm'] open form
+    //input[@value='No name photo'] add foto
+    //button[@class='sc-iyjcSV lkrIHE'] upload foto
+    //div[@class='sc-HUpva nvGQK']//div[@class='sc-fePcEy cZfHCq']//*[name()='svg'] close form comp
+    //button[normalize-space()='Save']  button save compani
 
+    //*[@id="root"]/div[1]/div[2]/div[1]/button return to
 }
